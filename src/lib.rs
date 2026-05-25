17093f57543cd3d59e1f7d62b0ff5c160a3e6ce2
LordAfterEight <elias@stettmayer.de>
2026-05-25 20:14:00 +0200

    Bump version to 0.1.6, split App into own submodule, add get_text_width function

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,17 @@
 mod canvas;
 pub mod color;
 pub mod font;
+pub mod app;
 
-pub struct App {
-    window: Option<std::sync::Arc<winit::window::Window>>,
-    pixels: Option<pixels::Pixels<'static>>,
-    title: String,
-    width: u32,
-    height: u32,
-    resizable: bool,
-    fullscreen: bool,
-    draw_fn: Option<Box<dyn FnMut(&mut canvas::Canvas)>>,
-}
+pub use app::App;
+pub use font::Font;
+pub use color::Color;
 
-impl App {
-    pub fn new(title: &str, width: u32, height: u32, resizable: bool, fullscreen: bool) -> Self {
-        Self {
-            window: None,
-            pixels: None,
-            title: title.to_string(),
-            width,
-            height,
-            resizable,
-            fullscreen,
-            draw_fn: None,
-        }
+pub fn get_text_width(text: &str, size: f32, font: &Font) -> u32 {
+    let mut width = 0;
+    for char in text.chars() {
+        let (metrics, _) = font.rasterize(char, size);
+        width += metrics.advance_width as u32;
     }
-
-    pub fn on_draw(&mut self, f: impl FnMut(&mut canvas::Canvas) + 'static) -> &mut Self {
-        self.draw_fn = Some(Box::new(f));
-        self
-    }
-
-    pub fn run(mut self) {
-        let event_loop = winit::event_loop::EventLoop::new().unwrap();
-        event_loop.run_app(&mut self).unwrap();
-    }
-}
-
-impl winit::application::ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let attributes = winit::window::Window::default_attributes()
-            .with_title(self.title.clone())
-            .with_resizable(self.resizable)
-            .with_inner_size(winit::dpi::PhysicalSize::new(self.width, self.height))
-            .with_fullscreen(
-                if self.fullscreen {
-                    Some(winit::window::Fullscreen::Borderless(None))
-                }
-                else {
-                    None
-                }
-            );
-        
-        let window = std::sync::Arc::new(event_loop.create_window(attributes).expect("Failed to create window"));
-
-        let surface_texture = pixels::SurfaceTexture::new(self.width, self.height, window.clone());
-        let pixels = pixels::Pixels::new(self.width, self.height, surface_texture).unwrap();
-        
-        self.pixels = Some(pixels);
-        self.window = Some(window);
-        self.window.as_ref().unwrap().request_redraw();
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    )
-    {
-        if let Some(window) = &self.window {
-            if window.id() == window_id {
-                match event {
-                    winit::event::WindowEvent::CloseRequested => {
-                        event_loop.exit();
-                    },
-                    winit::event::WindowEvent::RedrawRequested => {
-                        if let (Some(pixels), Some(draw_fn)) = (self.pixels.as_mut(), self.draw_fn.as_mut()) {
-                            let mut canvas = canvas::Canvas::new(pixels.frame_mut(), self.width, self.height);
-                            draw_fn(&mut canvas);
-                            pixels.render().unwrap();
-                        }
-                        self.window.as_ref().unwrap().request_redraw();
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
+    width
 }
